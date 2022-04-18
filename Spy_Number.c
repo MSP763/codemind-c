5e8436f38f91fa8c57a04cd0891177f487333b25
21A91A0583@aec.edu.in <97598280+MSP763@users.noreply.github.com>
2022-04-18 19:37:48 +0530

    #include<stdio.h> int main() {     int n,i,p=1,s=0;     scanf("%d",&n);     for(i=0;n>0;)     {       i=n%10;       n=n/10;       s=s+i;       p=p*i;     }    if(s==p)    {        printf("Spy Number");    }    else    {        printf("Not Spy Number");    } }

---
+++ Spy_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,p=1,s=0;
+    scanf("%d",&n);
+    for(i=0;n>0;)
+    {
+      i=n%10;
+      n=n/10;
+      s=s+i;
+      p=p*i;
+    }
+   if(s==p)
+   {
+       printf("Spy Number");
+   }
+   else
+   {
+       printf("Not Spy Number");
+   }
+} 
