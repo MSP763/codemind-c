17b71a0e03be28ef5ab5489a542ae20d5d9ab271
21A91A0583@aec.edu.in <97598280+MSP763@users.noreply.github.com>
2022-07-11 10:22:32 +0530

    #include<stdio.h> int main() {  int n1=0,n2=1,n3,i,number;  scanf("%d",&number);  for(i=2;i<number;++i)  {   n3=n1+n2;   if(n3==number)   {   printf("True");   break;   }   else if(n3>number)   {       printf("False");       break;   }   n1=n2;   n2=n3;  }   return 0;  }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,27 +1,23 @@
-#include<stdio.h>
-int main()
-{
-    int m,arr[100],i;
-    scanf("%d",&m);
-    arr[0]=0;
-    arr[1]=1;
-    for(i=2;i<100;i++)
-    {
-        arr[i]=arr[i-2]+arr[i-1];
-    }
-    for(i=0;i<100;i++)
-    {
-        if(m==arr[i])
-        {
-            printf("True");
-            break;
-        }
-        
-        else if(m<arr[i])
-        {
-            printf("False");
-            break;
-        }
-    }
-    return 0;
-}
+#include<stdio.h>    
+int main()    
+{    
+ int n1=0,n2=1,n3,i,number;    
+ scanf("%d",&number);    
+ for(i=2;i<number;++i)
+ {    
+  n3=n1+n2;    
+  if(n3==number)
+  {
+  printf("True");
+  break;
+  }
+  else if(n3>number)
+  {
+      printf("False");
+      break;
+  }
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+ }
