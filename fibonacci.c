8766d8b1d86050f06a80b1a1a01b317be9bd7ab1
21A91A0583@aec.edu.in <97598280+MSP763@users.noreply.github.com>
2022-07-11 10:23:27 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,c;     scanf("%d",&n);     printf("%d %d ",a,b);     for(int i=3;i<=n;i++)     {         c=a+b;         printf("%d ",c);         a=b;         b=c;     } }

--- fibonacci.c
+++ fibonacci.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int arr[100],n,i,a,b;
+    int n,a=0,b=1,c;
     scanf("%d",&n);
-    a=0;
-    b=1;
-    arr[0]=a;
-    arr[1]=b;
-    for(i=2;i<n;i++)
+    printf("%d %d ",a,b);
+    for(int i=3;i<=n;i++)
     {
-        arr[i]=arr[i-2]+arr[i-1];
-    }
-    for(i=0;i<n;i++)
-    {
-        printf("%d ",arr[i]);
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
     }
 }
